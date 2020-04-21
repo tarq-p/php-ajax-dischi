3470fdbc99966c7a5745273181502ee6c1d2bf23
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-21 15:59:34 +0200

    php ok

--- data.php
+++ data.php
@@ -1,52 +1,55 @@
 <?php
 
-$disco = [
-    [
-    "poster" =>"https://images-na.ssl-images-amazon.com/images/I/81nC3cAHSQL._AC_SL1500_.jpg"
-    "title" =>"Supersonic"
-    "author" =>"Oasis"
-    "genre" => "Rock&Roll"
-    ],
+    $dischi = [
+        [
+            "poster" =>"https://www.mbmusic.it/wp-content/uploads/2015/06/definitely-maybe-oasis-album-cover-300x300.jpg",
+            "title" =>"Supersonic",
+            "author" =>"Oasis",
+            "genre" => "Rock&Roll",
+            "year" => 1999,
+        ],
 
-    [
-    "poster" =>"https://a6p8a2b3.stackpathcdn.com/cEMKSw-aQHlm2B4nLvGE7Yfaka4=/300x300/smart/rockol-img/img/foto/upload/as-you-were-deluxe-edition-liam-gallagher-cover-ts1507270607.jpeg"
-    "title" =>"As you were"
-    "author" =>"Liam Gallagher"
-    "genre" => "Rock&Roll"
-    ],
+        [
+            "poster" =>"https://www.mbmusic.it/wp-content/uploads/2019/09/Liam-Gallagher-Why-Me-Why-Not-album-2019-cover.jpg",
+            "title" =>"As you were",
+            "author" =>"Liam Gallagher",
+            "genre" => "Rock&Roll",
+            "year" => 2018,
+        ],
 
-    [
-    "poster" =>"https://images-na.ssl-images-amazon.com/images/I/81WdTxsr2gL._AC_SX466_.jpg"
-    "title" =>"Drive in"
-    "author" =>"Blink182"
-    "genre" => "Rock"
-    ],
+        [
+            "poster" =>"https://www.mbmusic.it/wp-content/uploads/2016/08/x-ambassadors-vhs-album-2015.jpg",
+            "title" =>"Vhs",
+            "author" =>"X-Amabassadors",
+            "genre" => "Ambient",
+            "year" => 2017,
+        ],
 
-    "poster" =>"https://images-na.ssl-images-amazon.com/images/I/81WdTxsr2gL._AC_SX466_.jpg"
-    "title" =>"Drive in"
-    "author" =>"Blink182"
-    "genre" => "Rock"
-    ],
+        [
+            "poster" =>"https://images-na.ssl-images-amazon.com/images/I/81WdTxsr2gL._AC_SX466_.jpg",
+            "title" =>"Drive in",
+            "author" =>"Blink182",
+            "genre" => "Rock",
+            "year" => 2001,
+        ],
 
-    "poster" =>"https://img.discogs.com/gUXwSrm5X0-SrL527K9rP2wv8uU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-13469504-1555188199-3156.jpeg.jpg"
-    "title" =>"Live in Woodstock"
-    "author" =>"Greenday"
-    "genre" => "Rock"
-    ],
-
-    "poster" =>"https://img.discogs.com/gUXwSrm5X0-SrL527K9rP2wv8uU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-13469504-1555188199-3156.jpeg.jpg"
-    "title" =>"Live in Woodstock"
-    "author" =>"Greenday"
-    "genre" => "Rock"
-    ],
-
-    "poster" =>"https://img.discogs.com/gUXwSrm5X0-SrL527K9rP2wv8uU=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-13469504-1555188199-3156.jpeg.jpg"
-    "title" =>"Live in Woodstock"
-    "author" =>"Greenday"
-    "genre" => "Rock"
-    ],
+        [
+            "poster" =>"https://www.ravejungle.com/wp-content/uploads/2018/03/Hot-since-82.jpg",
+            "title" =>"Hot Since 82",
+            "author" =>"Hot Since 82",
+            "genre" => "House",
+            "year" => 2018,
+        ],
 
 
-];
+        [
+            "poster" =>"https://images-na.ssl-images-amazon.com/images/I/81nC3cAHSQL._AC_SL1500_.jpg",
+            "title" =>"As you were",
+            "author" =>"Liam Gallagher",
+            "genre" => "Rock&Roll",
+            "year" => 208,
+        ],
 
+    ];
+    // var_dump($dischi);
 ?>
--- index.php
+++ index.php
@@ -2,30 +2,39 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>ex-dischi-musicali</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script>
+        <title>php-ex-dischi-musicali</title>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.1.2/handlebars.min.js"></script> -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="dist/css/app.css">
     </head>
     <body>
         <header>
-            <div class="container">
+            <div class="container-header">
                 <img id="logo" src="logo.png" alt="logo"/>
             </div>
         </header>
 
-        <div class="cds-container container1">
-            <?php
-                include 'data.php';
-             ?>
+
+        <div class="cds-container container">
+            <?php include 'data.php'; ?>
+
+        <?php foreach ($dischi as $key => $disco) {?>
+          <div class="cd">
+            <img src="<?php echo $disco["poster"];?>" alt="">
+              <h3><?php echo $disco["title"];?></h3>
+              <span class="author"><?php echo $disco["author"];?></span>
+              <span class="year"><?php echo $disco["year"];?></span>
+          </div>
+        <?php } ?>
+
         </div>
 
+        <!-- <script id="cds-template" type="text/x-handlebars-template">
 
-        <script id="cds-template" type="text/x-handlebars-template">
-        
-        </script>
+        </script> -->
 
-        <script src="main.js" charset="utf-8"></script>
+        <script src="dist/js/main.js" charset="utf-8"></script>
+
     </body>
 </html>
